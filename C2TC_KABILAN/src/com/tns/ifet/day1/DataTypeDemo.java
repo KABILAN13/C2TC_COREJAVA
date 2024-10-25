2abb8c94470fbcd962a1cf5abd26780b51e9d83b
KABILAN13 <117358756+KABILAN13@users.noreply.github.com>
2024-10-25 15:17:27 +0530

    COMMIT

--- C2TC_KABILAN/src/com/tns/ifet/day1/DataTypeDemo.java
+++ C2TC_KABILAN/src/com/tns/ifet/day1/DataTypeDemo.java
@@ -2,7 +2,7 @@ package com.tns.ifet.day1;
 
 public class DataTypeDemo {
 	public static void main(String[] args) {
-		System.out.println("KABILAN REVATHI");
+		System.out.println("KABILAN");
 	}
 
 }
--- C2TC_KABILAN/src/sample/FirstProgram.java
+++ C2TC_KABILAN/src/sample/FirstProgram.java
@@ -1,5 +1,6 @@
 package sample;
 
 public class FirstProgram {
+	public static void main(Strin)
 
 }
