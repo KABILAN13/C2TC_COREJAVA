caba289fda69fdeb3e552aca659bf0b47f64a2c9
KABILAN13 <117358756+KABILAN13@users.noreply.github.com>
2024-11-06 11:53:23 +0530

    commit

---
+++ C2TC_KABILAN/src/com/tnsif/ifet/daysix/staticvariable/EmployeeDemo.java
@@ -0,0 +1,13 @@
+//Program to demonstrate static variables
+package com.tnsif.ifet.daysix.staticvariable;
+
+public class EmployeeDemo {
+	public static void main(String[] args) {
+	Employee e = new Employee("Shubh", 123);
+	System.out.println(e);
+	
+	// Similarly, create the second object of the class and pass the two arguments.
+	e = new Employee("Deep", 321);
+	System.out.println(e);
+}
+}
