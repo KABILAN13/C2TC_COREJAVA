d615e8bc5254ec0a48d66b102b37838b9956feca
KABILAN13 <117358756+KABILAN13@users.noreply.github.com>
2024-11-06 12:16:05 +0530

    commit

--- C2TC_KABILAN/src/com/tnsif/ifet/dayfive/multilevelinheritance/package-info.java
+++ C2TC_KABILAN/src/com/tnsif/ifet/dayfive/multilevelinheritance/package-info.java
@@ -1,2 +1,1 @@
 package com.tnsif.ifet.dayfive.multilevelinheritance;
-import java.util.Date;
--- C2TC_KABILAN/src/com/tnsif/ifet/daysix/usingfinal/FinalClassDemo.java
+++ C2TC_KABILAN/src/com/tnsif/ifet/daysix/usingfinal/FinalClassDemo.java
@@ -1,14 +1,10 @@
 package com.tnsif.ifet.daysix.usingfinal;
-
-final class FinalClass {
+class FinalClass {
 	void show() {
 		System.out.println("Final class cannot be inherited");
 	}
 }
 
-//can't create child classes from Final class, Ex: String, Wrapper Classes, System, Scanner, Number are Final classes
-class FinalChildClass extends FinalClass {
-}
 
 public class FinalClassDemo {
 	public static void main(String[] args) {
