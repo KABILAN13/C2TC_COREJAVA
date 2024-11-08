debc5a4059481545a0d8843b3ed0d522d487b8c9
KABILAN13 <117358756+KABILAN13@users.noreply.github.com>
2024-11-08 12:05:40 +0530

    commit

---
+++ C2TC_KABILAN/src/com/tnsif/dayseven/Point.java
@@ -0,0 +1,5 @@
+package com.tnsif.dayseven;
+
+public class Point {
+
+}
---
+++ C2TC_KABILAN/src/com/tnsif/ifet/dayseven/overriding/HDFC.java
@@ -0,0 +1,9 @@
+package com.tnsif.ifet.dayseven.overriding;
+
+public class HDFC extends RBI{
+	@Override
+	public float getRateOfInterest() {
+		return 6.8f;
+	}
+
+}
